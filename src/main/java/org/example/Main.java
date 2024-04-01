2634185104882d3cae26979efe7cfe217cc34a83
John Noble <j.nble92@gmail.com>
2024-04-01 09:13:09 -0400

    1st Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        List<Integer> arr = getRandArr();
+        Integer target = getRandNum();
+
+        System.out.println(Arrays.toString(getTwoSum(arr, target)));
+
+    }
+
+
+    public static int[] getTwoSum(List<Integer> arr, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.size(); i++) {
+            int comp = target - arr.get(i);
+            if (map.containsKey(comp)) {
+                return new int[] { map.get(comp), i};
+            }
+            map.put(arr.get(i), i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+
+    }
+
+
+
+    public static List<Integer> getRandArr(){
+        int arrMin = 2;
+        int arrMax = 30000;
+        int arrSize = (int)(Math.random() * ((arrMax - arrMin) + 1)) + arrMin;
+
+
+        List<Integer> randArr = new ArrayList<>();
+for(int i = 0; i < arrSize; i++){
+    Integer randNum = getRandNum();
+    randArr.add(randNum);
+
+}
+    return randArr;
+    }
+
+    public static Integer getRandNum(){
+        int valMin = -1000;
+        int valMax = 1000;
+        Integer num = (int)(Math.random() * ((valMax - valMin) + 1)) + valMin;
+
+        return num;
+    }
+
+
+
+    }
