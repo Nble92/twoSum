803952be98c3f15e60199a36abe2913a6f480812
John Noble <j.nble92@gmail.com>
2024-04-01 09:16:28 -0400

    1st Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         List<Integer> arr = getRandArr();
         Integer target = getRandNum();
 
